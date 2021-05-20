a0cde211e122fa207834d3f0326bf071e5e346cf
AgustinC13 <agustincannistraro76@hotmail.com>
2021-05-19 23:52:41 -0300

    Top-Down-Shooter

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public static float Puntos;
+
+
+    private void start ()
+    {
+        Puntos = 0;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+         
+            SceneManager.LoadScene("Derrota");
+        }
+        else if(other.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            Puntos += 1;
+        }
+
+        if (Puntos == 4)
+        {
+            SceneManager.LoadScene("Victoria");
+        }
+
+    }
+
+}
